5ea4d6d0574f6b70e6a628a0d5e8751e71271fe3
Shuhei Akiyama <popolocrois55@gmail.com>
2021-07-12 10:47:41 +0000

    add first version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles_centos
+
+## How to Setup
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/mikanbox/dotfiles_centos/main/install.sh"
+```
+
--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 
 
+
 # install 
 sudo yum install -y git
+sudo yum install source-highlight -y
 
 # set git editor to vim
 git config --global core.editor 'vim -c "set fenc=utf-8"'
+
+# get remote bashrc file
+wget https://raw.githubusercontent.com/mikanbox/dotfiles_centos/main/.bashrc
+
